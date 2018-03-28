d81457b0e7d2dc40123a09dc23b4110797eea3b9
Unknown <ezakimatova@gmail.com>
2018-03-28 10:14:11 +0300

    cleared up the code

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -310,7 +310,7 @@
  #define JucePlugin_Name                   "Vibrato-2"
 #endif
 #ifndef  JucePlugin_Desc
- #define JucePlugin_Desc                   "JUCE Project"
+ #define JucePlugin_Desc                   "Vibrato-2"
 #endif
 #ifndef  JucePlugin_Manufacturer
  #define JucePlugin_Manufacturer           "yourcompany"
@@ -325,7 +325,7 @@
  #define JucePlugin_ManufacturerCode       0x4d616e75 // 'Manu'
 #endif
 #ifndef  JucePlugin_PluginCode
- #define JucePlugin_PluginCode             0x55687965 // 'Uhye'
+ #define JucePlugin_PluginCode             0x5462766c // 'Tbvl'
 #endif
 #ifndef  JucePlugin_IsSynth
  #define JucePlugin_IsSynth                0
@@ -364,10 +364,10 @@
  #define JucePlugin_AUSubType              JucePlugin_PluginCode
 #endif
 #ifndef  JucePlugin_AUExportPrefix
- #define JucePlugin_AUExportPrefix         JUCEProjectAU
+ #define JucePlugin_AUExportPrefix         Vibrato2AU
 #endif
 #ifndef  JucePlugin_AUExportPrefixQuoted
- #define JucePlugin_AUExportPrefixQuoted   "JUCEProjectAU"
+ #define JucePlugin_AUExportPrefixQuoted   "Vibrato2AU"
 #endif
 #ifndef  JucePlugin_AUManufacturerCode
  #define JucePlugin_AUManufacturerCode     JucePlugin_ManufacturerCode
--- Source/Proc.h
+++ Source/Proc.h
@@ -10,23 +10,22 @@
 
 #pragma once
 
+#include "Utility.h"
 #include "Wavetable.h"
 #include "Ramp.h"
-#include "FractionalDelay.h"
 #include "Filter.h"
 
 class Proc
 {
 public:
-    Proc()  : sampleRate (44100), xFactor(4), minDelayInMs (0), maxDelayInMs (37), delayBuffer (0, 0),
+    Proc()  : xFactor(4), sampleRate (44100), minDelayInMs (0), maxDelayInMs (37), delayBuffer (0, 0),
               delayCounter(0), delayInSamples(0), prevProcessedLeft(0), prevProcessedRight(0)
     {
         //set initial values
-        
         lpFilter.frequency = 18000;
         
         //set wt parameters
-        wavetable.setAllParameters (0.1 / xFactor, 1, 0);
+        wavetable.setAllParameters (3.0f / (float) xFactor, 1, 0);
     }
     
     virtual ~Proc()
@@ -42,24 +41,22 @@ public:
         
         //prepare ramps
         //set time
-        delayTimeRamp.setTime (5, sampleRate);
         
         //prepare filters
         //count coefficients
         lpFilter.frequency = sampleRate / 2.4;
         lpFilter.countCoefficients (sampleRate * xFactor);
-        lpFilter1.frequency = lpFilter.frequency;
-        lpFilter1.countCoefficients (sampleRate * xFactor);
+        lpFilter1.frequency = sampleRate / 2.4;
+        lpFilter1.countCoefficients (sampleRate);
         
         //prepare ranges: set start, end, interval, skew
-        delayRange.start = 4;//Utility::msToSamplesF ((double) minDelayInMs, sampleRate);
+        delayRange.start = 4;
         delayRange.end = Utility::msToSamples ((double) maxDelayInMs, sampleRate * xFactor);
         
         //prepare wavetables
         //set sample rate, range & count wtt
         wavetable.setSampleRate (sampleRate);
         wavetable.setRange (delayRange);
-        //wavetable.setAllParameters (100, 500, 4, delayRange.end, 4, -2, 3, -20);
         wavetable.countWavetable();
         
         //prepare copy buffers
@@ -70,12 +67,10 @@ public:
     }
     
     Oscilator wavetable;
-    //Envelope wavetable;
     int xFactor;
     
 protected:
     double sampleRate;
-    Ramp delayTimeRamp;
     
     AntiAliasingFilter lpFilter;
     AntiAliasingFilter lpFilter1;
@@ -150,8 +145,8 @@ public:
             //get pointers to buffer
             const float* leftBufferR = xBuffer.getReadPointer(0);
             const float* rightBufferR = xBuffer.getReadPointer(1);
-            float* leftBufferW = xBuffer.getWritePointer (0);
-            float* rightBufferW = xBuffer.getWritePointer (1);
+            float* leftBufferW = buffer.getWritePointer (0);
+            float* rightBufferW = buffer.getWritePointer (1);
             
             //get pointers to delay buffer
             const float* leftDelayR = delayBuffer.getReadPointer(0);
@@ -168,35 +163,9 @@ public:
                                                    Utility::magnitude (inputRight)) / 2;
                 
                 //get current delay time
-//                delayTimeRamp.setRange (delayInSamples, wavetable.applyWavetable (averegeChannelLevel));
-//                delayTimeRamp.applyRamp (delayInSamples);
                 delayInSamples = wavetable.applyWavetable (averegeChannelLevel);
                 int delayInSamplesInt = (int) ceilf (delayInSamples);
                 float fraction = delayInSamples - floorf (delayInSamples);
-                
-//                if (fraction < 0.125)
-//                {
-//                    fraction = 0;
-//                }
-//                else if (fraction < 0.625)
-//                {
-//                    if (fraction < 0.375)
-//                        fraction = 0.25;
-//                    else
-//                        fraction = 0.5;
-//                }
-//                else
-//                {
-//                    if (fraction < 0.875)
-//                    {
-//                        fraction = 0.75;
-//                    }
-//                    else
-//                    {
-//                        fraction = 0;
-//                        delayInSamplesInt += 1;
-//                    }
-//                }
                 
                 //check counter
                 if (delayCounter >= circularBufferSize)
@@ -207,6 +176,8 @@ public:
                 int indexN = 0;
                 int indexN1 = 0;
                 int indexN2 = 0;
+                
+                //count indexes
                 
                 if (delayCounter >= delayInSamplesInt)
                     indexN = delayCounter - delayInSamplesInt;
@@ -229,13 +200,7 @@ public:
                     indexN2 = indexN1 + 1;
                 
                 
-//                float interpolatedLeft = Utility::linearInterpolation (fraction,
-//                                                                       leftDelayR [indexN],
-//                                                                       leftDelayR [indexN1]);
-//                
-//                float interpolatedRight = Utility::linearInterpolation (fraction,
-//                                                                        rightDelayR [indexN],
-//                                                                        rightDelayR [indexN1]);
+                //intepolate samples
                 
                 float interpolatedLeft = Utility::cubicInterpolation (fraction,
                                                                       leftDelayR [indexPrev],
@@ -248,32 +213,28 @@ public:
                                                                        rightDelayR [indexN],
                                                                        rightDelayR [indexN1],
                                                                        rightDelayR [indexN2]);
-                
+                //filter interpolated samples
                 float processedLeft = lpFilter.filterSignal (interpolatedLeft, 0);
                 float processedRight = lpFilter.filterSignal (interpolatedRight, 1);
                 
                 //output signal
-//                leftBufferW [sample] = processedLeft;
-//                rightBufferW [sample] = processedRight;
-                
-                
-                leftBufferW [sample] = lpFilter1.filterSignal (processedLeft, 0);
-                
-                rightBufferW [sample] = lpFilter1.filterSignal (processedRight, 1);
-                
-                prevProcessedLeft = processedLeft;
-                prevProcessedRight = processedRight;
-                
+                if (sample % xFactor == 0)
+                {
+                    leftBufferW [sample / xFactor] = lpFilter1.filterSignal (processedLeft, 0);
+                    rightBufferW [sample / xFactor] = lpFilter1.filterSignal (processedRight, 1);
+                }
+                    
                 //store input signal in the delay buffer
                 leftDelayW [delayCounter] = inputLeft + 0.9 * processedLeft;
                 rightDelayW [delayCounter] = inputRight + 0.9 * processedRight;
                 
+                //store previous values
+                prevProcessedLeft = processedLeft;
+                prevProcessedRight = processedRight;
+                
                 //increase counter
                 delayCounter++;
             }
-            
-            Utility::downsample (xBuffer, buffer, xFactor);
-            //Utility::oversample (xBuffer, buffer, xFactor);
         }
         else
         {
